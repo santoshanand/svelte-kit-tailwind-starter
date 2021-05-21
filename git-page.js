f1aa98c9e81290e29c41a44b9f17ceecae3f13b8
Santosh Anand <santosh@Santoshs-MacBook-Pro.local>
2021-05-21 21:35:58 +0530

    Added gh-pages

--- git-page.js
+++ git-page.js
@@ -1,8 +1,5 @@
 // var ghpages = require('gh-pages');
 import * as ghpages from 'gh-pages'
-ghpages.publish('build', {
-  branch: 'master',
-  repo: 'https://github.com/santoshanand/svelte-kit-tailwind-starter.git'
-}, function(e) {
+ghpages.publish('build', function(e) {
   console.log(e)
 });
